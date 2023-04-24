11c093c1d145a4c9fba360e9bee49af57cbd4810
vergotten <vergotten@gmail.com>
2023-04-24 21:24:02 +0300

    model gpt-3.5-turbo

--- src/pages/digimishka.jsx
+++ src/pages/digimishka.jsx
@@ -62,7 +62,7 @@ function DigimishkaPage() {
     // and the messages which we formatted above. We add a system message in the front to'
     // determine how we want chatGPT to act.
     const apiRequestBody = {
-        "model": "gpt-3.5-turbo-0301",
+        "model": "gpt-3.5-turbo",
         "messages": [
             systemMessage,  // The system message DEFINES the logic of our chatGPT
             ...apiMessages // The messages from our chat with ChatGPT
