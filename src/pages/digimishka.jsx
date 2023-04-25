365e4a2ff0a961abbc821cb691a5cc562b55c5e4
vergotten <vergotten@gmail.com>
2023-04-25 23:41:04 +0300

    fixed chat width

--- src/pages/digimishka.jsx
+++ src/pages/digimishka.jsx
@@ -92,7 +92,7 @@ function DigimishkaPage() {
     return (
         <Layout pageTitle="Chat with Digimishka">
             <div className="App">
-                <div style={{ position: "relative", height: "490px", width: "380px" }}>
+                <div style={{ position: "relative", height: "490px", width: "375px" }}>
                     <MainContainer>
                         <ChatContainer>
                             <MessageList
