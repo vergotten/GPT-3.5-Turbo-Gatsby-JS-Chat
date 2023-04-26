0ff1c7a5eaae8f934e9f6865ff1bc19e3d643e8f
vergotten <vergotten@gmail.com>
2023-04-27 00:07:02 +0300

    Test Chat

---
+++ src/components/Chat/Chat.jsx
@@ -0,0 +1,53 @@
+import { Flex } from "@chakra-ui/react";
+import * as React from 'react'
+import { useState } from "react";
+import Divider from "../Chat/Divider";
+import Footer from "../Chat/Footer";
+import Header from "./Chat/Header";
+import Messages from "../Chat/Messages";
+
+const Chat = () => {
+  const [messages, setMessages] = useState([
+    { from: "computer", text: "Hi, My Name is HoneyChat" },
+    { from: "me", text: "Hey there" },
+    { from: "me", text: "Myself Ferin Patel" },
+    {
+      from: "computer",
+      text:
+        "Nice to meet you. You can send me message and i'll reply you with same message."
+    }
+  ]);
+  const [inputMessage, setInputMessage] = useState("");
+
+  const handleSendMessage = () => {
+    if (!inputMessage.trim().length) {
+      return;
+    }
+    const data = inputMessage;
+
+    setMessages((old) => [...old, { from: "me", text: data }]);
+    setInputMessage("");
+
+    setTimeout(() => {
+      setMessages((old) => [...old, { from: "computer", text: data }]);
+    }, 1000);
+  };
+
+  return (
+    <Flex w="100%" h="100vh" justify="center" align="center">
+      <Flex w={["100%", "100%", "40%"]} h="90%" flexDir="column">
+        <Header />
+        <Divider />
+        <Messages messages={messages} />
+        <Divider />
+        <Footer
+          inputMessage={inputMessage}
+          setInputMessage={setInputMessage}
+          handleSendMessage={handleSendMessage}
+        />
+      </Flex>
+    </Flex>
+  );
+};
+
+export default Chat;
--- src/pages/TestChat.jsx
+++ src/pages/TestChat.jsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { ChakraProvider, theme } from "@chakra-ui/react";
-import Chat from "../components/Chat";
+import Chat from "../components/Chat/Chat";
 import './App.css'
 
 const App = () => {
