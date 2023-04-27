e4943f509af5321e5ecaaea2c572fc681412b4df
vergotten <vergotten@gmail.com>
2023-04-27 14:36:56 +0300

    Test Chat

--- src/components/Chat/Header.jsx
+++ src/components/Chat/Header.jsx
@@ -4,7 +4,7 @@ import { Flex, Avatar, AvatarBadge, Text } from "@chakra-ui/react";
 const Header = () => {
   return (
     <Flex w="100%">
-      <Avatar size="lg" name="Dan Abrahmov" src="../../assets/icons/user.png">
+      <Avatar size="lg" name="Dan Abrahmov" src="https://i.gyazo.com/d4e08b9a9e63697be15334e23078bf18.png">
         <AvatarBadge boxSize="1.25em" bg="green.500" />
       </Avatar>
       <Flex flexDirection="column" mx="5" justify="center">
