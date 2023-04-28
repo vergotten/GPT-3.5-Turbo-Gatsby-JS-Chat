6cdea4b0c34c22fe5b22ab23d31bc3e9a97612b9
vergotten <vergotten@gmail.com>
2023-04-28 18:35:10 +0300

    MyChat

--- src/components/MyChat/Header.jsx
+++ src/components/MyChat/Header.jsx
@@ -5,13 +5,16 @@ const Header = () => {
   return (
     <Flex w="100%">
       <Avatar size="lg" name="Digimishka" src="https://i.gyazo.com/01022bae8569c411593ef308d1f8eb56.jpg">
-        <AvatarBadge boxsize="1.25em" bg="green.500" />
+        <AvatarBadge boxSize="1.25em" bg="green.500" />
       </Avatar>
       <Flex flexDirection="column" mx="5" justify="center">
-        <Text>
+        <Text fontSize="lg" fontWeight="bold">
           Digimishka
         </Text>
         <Text color="green.500">Online</Text>
       </Flex>
+    </Flex>
   );
 };
+
+export default Header;
