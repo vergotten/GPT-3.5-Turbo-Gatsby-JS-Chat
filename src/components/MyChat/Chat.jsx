523190f23ba319deb4a52caff0535d8d282dd079
vergotten <vergotten@gmail.com>
2023-04-28 21:20:29 +0300

    MyChat

--- src/components/MyChat/Chat.jsx
+++ src/components/MyChat/Chat.jsx
@@ -68,32 +68,32 @@ const MyChat = () => {
     return { role: role, content: messageObject.message }
   });
 
-    const apiRequestBody = {
-      "model": "gpt-3.5-turbo",
-      "messages": [
-        systemMessage,
-        ...apiMessages
-      ]
-    }
+  const apiRequestBody = {
+    "model": "gpt-3.5-turbo",
+    "messages": [
+      systemMessage,
+      ...apiMessages
+    ]
+  }
 
-    await fetch("https://api.openai.com/v1/chat/completions",
-    {
-      method: "POST",
-      headers: {
-        "Authorization": "Bearer " + process.env.OPENAI_KEY,
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(apiRequestBody)
-    }).then((data) => {
-      return data.json();
-    }).then((data) => {
-      console.log(data);
-      setMessages([...chatMessages, {
-        message: data.choices[0].message.content,
-        sender: "Digimishka"
-        }]);
-      setIsTyping(false);
-    });
+  await fetch("https://api.openai.com/v1/chat/completions",
+  {
+    method: "POST",
+    headers: {
+      "Authorization": "Bearer " + process.env.OPENAI_KEY,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(apiRequestBody)
+  }).then((data) => {
+    return data.json();
+  }).then((data) => {
+    console.log(data);
+    setMessages([...chatMessages, {
+      message: data.choices[0].message.content,
+      sender: "Digimishka"
+      }]);
+    setIsTyping(false);
+  });
   }
 
   return (
