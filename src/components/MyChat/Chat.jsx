f5b918c686e50dbd837534daab55915c840f0194
vergotten <vergotten@gmail.com>
2023-04-28 17:23:39 +0300

    MyChat

--- src/components/MyChat/Chat.jsx
+++ src/components/MyChat/Chat.jsx
@@ -4,13 +4,13 @@ import { useState } from 'react';
 import Header from "./Header.jsx";
 
 const MyChat = () => {
-    return (
-        <Flex w="100%" h="100vh" justify="center" align="center">
-            <Flex w={["100%", "100%", "40%"]} h="90%" flexDir="column">
-                <Header />
-            </Flex>
-        </Flex>
-    );
+  return (
+    <Flex w="100%" h="100vh" justify="center" align="center">
+      <Flex w={["100%", "100%", "40%"]} h="90%" flexDir="column">
+        <Header />
+      </Flex>
+    </Flex>
+  );
 };
 
 export default MyChat;
