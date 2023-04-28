fad6e24c9387a5cac22f4b4e4cf4ae9df46dc223
vergotten <vergotten@gmail.com>
2023-04-28 18:52:00 +0300

    MyChat

--- src/pages/MyChat.js
+++ src/pages/MyChat.js
@@ -1,4 +1,4 @@
-import * as React form 'react';
+import * as React from 'react';
 import { ChakraProvider, theme } from "@chakra-ui/react";
 import Chat from "../components/MyChat/Chat";
 import "./MyChat.css"
