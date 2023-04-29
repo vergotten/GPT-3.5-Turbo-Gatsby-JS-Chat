936df7cddcc6a9d5a648f8a52bbe1de7f52516e3
vergotten <vergotten@gmail.com>
2023-04-29 18:32:45 +0300

    TestChat

--- src/components/MyChat/Chat.js
+++ src/components/MyChat/Chat.js
@@ -42,7 +42,7 @@ const Chat = () => {
     await processMessageToChatGPT(newMessages);
   };
 
-  async function processMessageToChatGPT(chatMessages) { // messages is an array of messages
+  async function processMessageToChatGPT(chatMessages) { // chatMessages is an array of messages
     // Format messages for chatGPT API
     // API is expecting objects in format of { role: "user" or "assistant", "content": "message here"}
     // So we need to reformat
@@ -54,39 +54,39 @@ const Chat = () => {
       } else {
         role = "user";
       }
-      return { role: role, content: messageObject.message }
-    });
-
-    // Get the request body set up with the model we plan to use
-    // and the messages which we formatted above. We add a system message in the front to'
-    // determine how we want chatGPT to act.
-    const apiRequestBody = {
-        "model": "gpt-3.5-turbo",
-        "messages": [
-            systemMessage,  // The system message DEFINES the logic of our chatGPT
-            ...apiMessages // The messages from our chat with ChatGPT
-      ]
-    };
-
-    await fetch("https://api.openai.com/v1/chat/completions",
-    {
-      method: "POST",
-      headers: {
-        "Authorization": "Bearer " + process.env.OPENAI_KEY,
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(apiRequestBody)
-    }).then((data) => {
-        return data.json();
-    }).then((data) => {
-      console.log(data);
-      setMessages([...chatMessages, {
-        text: data.choices[0].message.content,
-        from: "Digimishka"
-      }]);
-      setIsTyping(false);
+    return { role: role, content: messageObject.message }
     });
   };
+
+  // Get the request body set up with the model we plan to use
+  // and the messages which we formatted above. We add a system message in the front to'
+  // determine how we want chatGPT to act.
+  const apiRequestBody = {
+      "model": "gpt-3.5-turbo",
+      "messages": [
+          systemMessage,  // The system message DEFINES the logic of our chatGPT
+          ...apiMessages // The messages from our chat with ChatGPT
+    ]
+  };
+
+  await fetch("https://api.openai.com/v1/chat/completions",
+  {
+    method: "POST",
+    headers: {
+      "Authorization": "Bearer " + process.env.OPENAI_KEY,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(apiRequestBody)
+  }).then((data) => {
+      return data.json();
+  }).then((data) => {
+    console.log(data);
+    setMessages([...chatMessages, {
+      text: data.choices[0].message.content,
+      from: "Digimishka"
+    }]);
+    setIsTyping(false);
+  });
 
   return (
     <Flex w="100%" h="100vh" justify="center" align="center">
