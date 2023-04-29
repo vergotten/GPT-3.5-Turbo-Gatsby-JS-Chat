b2080b8199d36443ac823d88d857f7dbde8dd88d
vergotten <vergotten@gmail.com>
2023-04-29 21:08:37 +0300

    TestChat

--- src/pages/digimishka.js
+++ src/pages/digimishka.js
@@ -10,61 +10,61 @@ import { MainContainer, ChatContainer, MessageList, Message, MessageInput, Typin
 // "Explain things like you would to a 10 year old learning how to code."
 // "Explain things like you're talking to a software professional with 2 years of experience."
 const systemMessage = {
-    role: "system",
-    content: "Explain things like you're talking to a software professional with 2 years of experience."
+  role: "system",
+  content: "Explain things like you're talking to a software professional with 2 years of experience."
 }
 
 function DigimishkaPage() {
-    const [messages, setMessages] = useState([
-        {
-            message: "Hello, I am Digimishka!",
-            sentTime: "just now",
-            sender: "Digimishka"
-        }
-    ]);
-    const [isTyping, setIsTyping] = useState(false);
+  const [messages, setMessages] = useState([
+    {
+      message: "Hello, I am Digimishka!",
+      sentTime: "just now",
+      sender: "Digimishka"
+    }
+  ]);
+  const [isTyping, setIsTyping] = useState(false);
 
-    const handleSend = async (message) => {
-        const newMessage = {
-            message,
-            direction: 'outgoing',
-            sender: "user"
-        };
-
-        const newMessages = [...messages, newMessage]; // all the old messages, + the new message
-
-        // update our messages state
-        setMessages(newMessages);
-
-        // set a typing indicator (Digimishka is typing)
-        setIsTyping(true);
-        // process message to Digimishka (send it over and see the response)
-        await processMessageToChatGPT(newMessages);
+  const handleSend = async (message) => {
+    const newMessage = {
+      message,
+      direction: 'outgoing',
+      sender: "user"
     };
 
-    async function processMessageToChatGPT(chatMessages) { // messages is an array of messages
-        // Format messages for chatGPT API
-        // API is expecting objects in format of { role: "user" or "assistant", "content": "message here"}
-        // So we need to reformat
+    const newMessages = [...messages, newMessage]; // all the old messages, + the new message
 
-        let apiMessages = chatMessages.map((messageObject) => {
-            let role = "";
-            if (messageObject.sender === "Digimishka") {
-                role = "assistant";
-            } else {
-                role = "user";
-        }
-        return { role: role, content: messageObject.message}
+    // update our messages state
+    setMessages(newMessages);
+
+    // set a typing indicator (Digimishka is typing)
+    setIsTyping(true);
+    // process message to Digimishka (send it over and see the response)
+    await processMessageToChatGPT(newMessages);
+  };
+
+  async function processMessageToChatGPT(chatMessages) { // messages is an array of messages
+    // Format messages for chatGPT API
+    // API is expecting objects in format of { role: "user" or "assistant", "content": "message here"}
+    // So we need to reformat
+
+    let apiMessages = chatMessages.map((messageObject) => {
+      let role = "";
+      if (messageObject.sender === "Digimishka") {
+        role = "assistant";
+      } else {
+        role = "user";
+      }
+      return { role: role, content: messageObject.message}
     });
 
     // Get the request body set up with the model we plan to use
     // and the messages which we formatted above. We add a system message in the front to'
     // determine how we want chatGPT to act.
     const apiRequestBody = {
-        "model": "gpt-3.5-turbo",
-        "messages": [
-            systemMessage,  // The system message DEFINES the logic of our chatGPT
-            ...apiMessages // The messages from our chat with ChatGPT
+      "model": "gpt-3.5-turbo",
+      "messages": [
+        systemMessage,  // The system message DEFINES the logic of our chatGPT
+        ...apiMessages // The messages from our chat with ChatGPT
       ]
     }
 
@@ -88,27 +88,28 @@ function DigimishkaPage() {
     });
   }
 
-    return (
-        <Layout pageTitle="Chat with Digimishka">
-            <div className="App">
-                <div style={{ position: "relative", height: "100%", width: "100%" }}>
-                    <MainContainer>
-                        <ChatContainer>
-                            <MessageList
-                                scrollBehavior="smooth"
-                                typingIndicator={isTyping ? <TypingIndicator content="Digimishka is typing" /> : null}
-                            >
-                                {messages.map((message, i) => {
-                                    return <Message key={i} model={message} />
-                                })}
-                            </MessageList>
-                            <MessageInput placeholder='Type message here' onSend={handleSend} />
-                        </ChatContainer>
-                    </MainContainer>
-                </div>
-            </div>
-        </Layout>
-    )
+  return (
+    <Layout pageTitle="Chat with Digimishka">
+      <div className="App">
+        <div style={{ position: "relative", height: "100%", width: "100%" }}>
+          <MainContainer>
+            <ChatContainer>
+              <MessageList
+                scrollBehavior="smooth"
+                typingIndicator={isTyping ? <TypingIndicator content="Digimishka is typing" /> : null}
+              >
+                { messages.map((message, i) => {
+                  console.log(message)
+                  return <Message key={i} model={message} />
+                })}
+              </MessageList>
+              <MessageInput placeholder='Type message here' onSend={handleSend} />
+            </ChatContainer>
+          </MainContainer>
+        </div>
+      </div>
+    </Layout>
+  )
 }
 
 export const Head = () => <title>Chat with Digimishka</title>
